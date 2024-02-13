6836b36c6aab69acf106c97e9c2699284383b4df
Rax <Rax@DESKTOP-41U8OV7>
2024-02-13 15:58:09 +0300

    Add new Basket and Order controllers

---
+++ WebApplication1/Controllers/BasketController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebShop.Service.Interfaces;
+
+namespace WebApplication.Controllers
+{
+    public class BasketController : Controller
+    {
+        private readonly IBasketService _basketService;
+
+        public BasketController(IBasketService basketService)
+        {
+            _basketService = basketService;
+        }
+
+        [Authorize]
+        [HttpGet]
+        public async Task<IActionResult> Detail()
+        {
+            var response = await _basketService.GetItems(User.Identity.Name);
+            if (response.StatusCode == WebShop.Domain.Enum.StatusCode.OK)
+            {
+                return View(response.Data.ToList());
+            }
+            return RedirectToAction("Index", "Home");
+        }
+
+        [Authorize]
+        [HttpGet]
+        public async Task<IActionResult> GetItem(Guid id)
+        {
+            var response = await _basketService.GetItem(User.Identity.Name, id);
+            if (response.StatusCode == WebShop.Domain.Enum.StatusCode.OK)
+            {
+                return View(response.Data);
+            }
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
---
+++ WebApplication1/Controllers/OrderController.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebShop.Domain.ViewModels.Order;
+using WebShop.Service.Interfaces;
+
+namespace WebApplication.Controllers
+{
+    [Authorize]
+    public class OrderController : Controller
+    {
+        private readonly IOrderService _orderService;
+
+        public OrderController(IOrderService orderService)
+        {
+            _orderService = orderService;
+        }
+
+        [HttpGet]
+        public IActionResult CreateOrder(Guid id)
+        {
+            var orderModel = new CreateOrderViewModel()
+            {
+                ProductId = id,
+                Login = User.Identity.Name,
+                Quantity = 0
+            };
+            return View(orderModel);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateOrder(CreateOrderViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var response = await _orderService.Create(model);
+                if (response.StatusCode == WebShop.Domain.Enum.StatusCode.OK)
+                {
+                    return Json(new { description = response.DescriptionError });
+                }
+            }
+            return StatusCode(StatusCodes.Status500InternalServerError);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Delete(Guid id)
+        {
+            var response = await _orderService.Delete(id);
+            if (response.StatusCode == WebShop.Domain.Enum.StatusCode.OK)
+            {
+                return RedirectToAction("Index", "Home");
+            }
+            return View("Error", $"{response.DescriptionError}");
+        }
+    }
+}
