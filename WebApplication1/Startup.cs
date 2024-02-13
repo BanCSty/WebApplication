25758e0814a3775c3cb712824090482810ff7ab3
Rax <Rax@DESKTOP-41U8OV7>
2024-02-13 20:00:49 +0300

    Change sturtup

--- WebApplication1/Startup.cs
+++ WebApplication1/Startup.cs
@@ -60,54 +60,7 @@ namespace WebApplication
             }).AddCookie(x =>
             {
                 x.Cookie.Name = "Bearer";
-                x.Events = new CookieAuthenticationEvents
-                {
-                    OnValidatePrincipal = async x =>
-                    {
-
-                        // since our cookie lifetime is based on the access token one,
-                        // check if we're more than halfway of the cookie lifetime
-                        var now = DateTimeOffset.UtcNow;
-                        var timeElapsed = now.Subtract(x.Properties.IssuedUtc.Value);
-                        var timeRemaining = x.Properties.ExpiresUtc.Value.Subtract(now);
-
-                        if (timeElapsed > timeRemaining)
-                        {
-                            var identity = (ClaimsIdentity)x.Principal.Identity;
-                            var accessTokenClaim = identity.FindFirst("access_token");
-                            var refreshTokenClaim = identity.FindFirst("refresh_token");
-
-                            // if we have to refresh, grab the refresh token from the claims, and request
-                            // new access token and refresh token
-                            var refreshToken = refreshTokenClaim.Value;
-                            var response = await new HttpClient().RequestRefreshTokenAsync(new RefreshTokenRequest
-                            {
-                                Address = "https://localhost:44314/Account/RefreshToken",
-                                ClientId = "mvc",
-                                ClientSecret = "mvc",
-                                RefreshToken = refreshToken
-                            });
-
-                            if (!response.IsError)
-                            {
-                                // everything went right, remove old tokens and add new ones
-                                identity.RemoveClaim(accessTokenClaim);
-                                identity.RemoveClaim(refreshTokenClaim);
-
-                                identity.AddClaims(new[]
-                                {
-                                        new Claim("access_token", response.AccessToken),
-                                        new Claim("refresh_token", response.RefreshToken)
-                                    });
-
-                                // indicate to the cookie middleware to renew the session cookie
-                                // the new lifetime will be the same as the old one, so the alignment
-                                // between cookie and access token is preserved
-                                x.ShouldRenew = true;
-                            }
-                        }
-                    }
-                };
+                
             })
             .AddJwtBearer(options =>
             {
